a9643bbec5a657423bc44a4b8bf536b5383b9550
gustavo <gu.losada@hotmail.com>
2022-02-19 12:28:46 -0300

    retomar da secao 1.2, falando de tipos

---
+++ index.js
@@ -0,0 +1,35 @@
+var idade = 18
+console.log ("Oi, " + nome)
+if (idade >= 18){
+    //var nome = "João"
+    let nome = "João"
+    console.log ("Parabéns João")
+    console.log ("Você pode dirigir")
+}
+console.log ("Até mais " + nome)
+
+
+
+
+//var linguagem  = "javascript"
+//console.log ("Aprendendo" + linguagem)
+//variavel pode ser redeclarada
+//linguagem = "java"
+//console.log ("Aprendendo " + linguagem)
+
+
+
+//const nome = 'José "" '
+//const endereco = `Rua K, 12`
+//const sexo = "F ' ' "
+//const idade = 27
+
+//let a = 2
+//a = 3
+//let nomeCompleto = "Joao Silva"
+//a = 'abc'
+
+
+//var c = 2 + 3
+//c = 6
+//var d = "abc"
