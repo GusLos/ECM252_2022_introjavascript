725d192984b641b36174d59b9d9eead46fe041a9
gustavo <gu.losada@hotmail.com>
2022-02-26 12:07:12 -0300

    Retomar da secao 1.6 a apostila 2, falando sobre funcoes.

--- index.js
+++ index.js
@@ -1,12 +1,168 @@
-var idade = 18
-console.log ("Oi, " + nome)
-if (idade >= 18){
-    //var nome = "João"
-    let nome = "João"
-    console.log ("Parabéns João")
-    console.log ("Você pode dirigir")
-}
-console.log ("Até mais " + nome)
+
+const valores = [1, 2, 3, 4]
+const soma = valores.reduce ((acumulador, atual) => {
+    return acumulador + atual
+})
+console.log(soma)
+
+
+
+
+
+
+
+
+
+
+// const nomes = [
+//     'Ana Maria',
+//     'Antonio',
+//     'Rodrigo',
+//     'Alex',
+//     'Cristina'
+// ]
+
+//Encontrar todos os nomes que começam com a
+
+// filter(funcao){
+//     funcao()
+// }
+
+// arrow function           // usado para declarar função
+// () => { }                // função sem nome 
+
+
+// const apenasA = nomes.filter((n) => {
+//     return n.startsWith("A")
+// })
+
+// console.log(apenasA)
+
+// obter [A, A, R, A, C]
+
+// const mapeamento = nomes.map((nome) => {
+//     return nome.charAt(0)
+// })
+
+// console.log(mapeamento)
+
+
+
+// const resultado = nomes.every((n) => {
+//     return n.startsWith("A")
+// })
+// console.log(resultado)
+
+
+
+
+
+
+
+
+// const a = "ab cd"
+// console.log(a[0])
+// console.log(a[1])
+// console.log(a[2])
+// console.log(a[3])
+// console.log(a[4])
+
+
+
+
+
+
+// const v2 = [2, 'abc', true]
+
+// const v1 = []
+// //console.log(v1.length)
+// v1[0] = 3.4
+// v1[2] = 'abc'
+// v1[10] = 2
+// console.log(v1.length)
+
+// for (let i = 0; i < v1.length; i++){
+//     console.log(v1[i])
+// }
+// console.log(v1[5] === v1[6])
+
+
+
+
+// =   atribuição
+// ==  comparação (com coerção implícita)  [geralmente não usa]
+// === comparação (sem coerção implícita, compara por tipo e valor)
+
+// console.log(1 == 1)
+
+//console.log(1 == '1')
+
+// console.log(1 === 1)
+
+//console.log(1 === '1')          // compara por tipo e depois por valor -> Number === Number ->  1 === 1 
+
+// console.log(false == 0)
+
+// console.log(true === 1)
+
+// console.log(1 == ['1'])
+// console.log(1 == [1])
+
+// console.log(1 === [1])
+
+// console.log(null == null)
+// console.log(null == undefined)
+
+// console.log(false == null)
+// console.log(false == undefined)
+
+// console.log([] == false)
+
+// console.log([] == [])    // mesma propriedade porém oobjetos diferentes 
+// console.log([] === [])
+
+// const a = []
+// const b = a              // apontam para o mesmo objeto
+// console.log(a == b) 
+// console.log(a === b)
+
+//console.log([2] == [2])   // comparação == pode variar nos valores, as vezes true == true pode resultar em false 
+
+
+
+
+
+
+
+// const n1 = 2
+// const n2 = '3'
+// //coerção implícita
+// const n3 = n1 + n2 
+// //n3 = n1 - n2
+// //n3 = n1 * n2
+// console.log(n3)
+// //coerção explícita 
+// const n4 = n1 + Number(n2) // == n4 = n1 + +n2
+// console.log(n4)
+
+
+
+// let a = null
+// console.log(a)
+// console.log(typeof(a))
+// a = "abc"
+
+
+
+// var idade = 18
+// console.log ("Oi, " + nome)
+// if (idade >= 18){
+//    //var nome = "João"
+//    let nome = "João"
+//    console.log ("Parabéns João")
+//    console.log ("Você pode dirigir")
+// }
+// console.log ("Até mais " + nome)
 
 
 
